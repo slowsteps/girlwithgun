81a468338ddfb2bca49d4d7a16eea40161ae9cc9
peter <hofstedemail@yahoo.com>
2013-01-20 22:53:17 +0100

    about page fixes: ads removed and header now above the rest of the post

--- page.php
+++ page.php
@@ -31,56 +31,18 @@ wp_reset_postdata();
 
 <div id="midcol">
 	<?php 
-		the_title("<h2 class=\"singlepostheader\">","</h2>");
+		the_title("<h2 class=\"pageheader\">","</h2>");
 		the_content();
-		the_category(' '); 
 		the_tags("<span class=\"tagbutton\"  onclick=\"_gaq.push(['_trackEvent', 'tag', 'clicked'])\"   >","</span><span onclick=\"_gaq.push(['_trackEvent', 'tag', 'clicked'])\" class=\"tagbutton\">","</span>");	
-
-
 		wp_reset_postdata();
-
-	    $args = array( 'post_type' => 'attachment', 'post_parent' => $post->ID,  'post_mime_type' => 'application/x-shockwave-flash', 'numberposts' => 1  ); 
-   	 	$attachments = get_posts($args);
-    	if ($attachments) {
-            foreach ( $attachments as $attachment ) {  
-                //echo $attachment->guid;
-            }
-    	}
-    	//else echo "no attachments";
-    	
-		//get the width and height of the game from the editor custom fields
-		$customfieldwidth = get_post_custom_values('swf-width', $post->ID);
-		$gamewidth = $customfieldwidth[0];
-		//echo($gamewidth);
-		$customfieldheight = get_post_custom_values('swf-height', $post->ID);
-		$gameheight = $customfieldheight[0];
-		//echo($gameheight);
-
 	?>
 	
-	<script type="text/javascript">
-    	swfobject.embedSWF("<?php echo $attachment->guid; ?>", "myContent", "<?php echo $gamewidth;?>", "<?php echo $gameheight;?>", "11.0.0");
-    </script>
-
-	<p>
-	<div id="myContent">
-    </div>
-	</p>
+	
 	
 </div>
 
 <div id="rightcol">	
-	<script type="text/javascript"><!--
-	google_ad_client = "ca-pub-4715818108855319";
-	/* girlwithgun skyscraper */
-	google_ad_slot = "5069061423";
-	google_ad_width = 160;
-	google_ad_height = 600;
-	//-->
-	</script>
-	<script type="text/javascript"
-	src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-	</script>
+
 </div>
 
 
