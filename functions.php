f556a11bf5e1187d5352038bb937087fcc8dd61b
peter <hofstedemail@yahoo.com>
2013-01-27 22:59:37 +0100

    removed orig-with

--- functions.php
+++ functions.php
@@ -14,8 +14,8 @@ function add_custom_field_automatically($post_ID) {
         add_post_meta($post_ID, 'swf-height', 'custom value', true);
         add_post_meta($post_ID, 'swf-width', 'custom value', true);
 
-        add_post_meta($post_ID, 'orig-width', 'custom value', true);
-        add_post_meta($post_ID, 'orig-height', 'custom value', true);
+        //add_post_meta($post_ID, 'orig-width', 'custom value', true);
+        //add_post_meta($post_ID, 'orig-height', 'custom value', true);
 
         add_post_meta($post_ID, 'meta-description', 'custom value', true);
 
@@ -31,8 +31,8 @@ function mk_set_default_custom_fields($post_id)
         add_post_meta($post_id, 'swf-width', '', true);
         add_post_meta($post_id, 'swf-height', '', true);
 
-        add_post_meta($post_ID, 'orig-width', 'custom value', true);
-        add_post_meta($post_ID, 'orig-height', 'custom value', true);
+        //add_post_meta($post_ID, 'orig-width', 'custom value', true);
+        //add_post_meta($post_ID, 'orig-height', 'custom value', true);
 
         add_post_meta($post_ID, 'meta-description', 'custom value', true);
 
