b6737824879cc35d1b2dc867ef935c1017569b4b
peter <hofstedemail@yahoo.com>
2013-04-13 15:18:49 +0200

    leaderboard

--- footer.php
+++ footer.php
@@ -21,6 +21,22 @@
 
 <div id="footer" class="backgroundcolor">
 
+	<div id="leaderboard">
+		<script type="text/javascript"><!--
+		google_ad_client = "ca-pub-4715818108855319";
+		/* Leaderboard */
+		google_ad_slot = "4329799079";
+		google_ad_width = 728;
+		google_ad_height = 90;
+		//-->
+		</script>
+		<script type="text/javascript"
+		src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+		</script>
+	</div>
+
+
+
 	<div id="tagcloud">
 
 		<?php 
